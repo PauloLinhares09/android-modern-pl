7c58349acbe25ce7fae63ec0dffb5177c23e21d1
Paulo Linhares <inf00174@sky.com.br>
2024-03-29 12:57:16 -0300

    app init

---
+++ app/src/main/java/com/packapps/android_modern_pl/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.packapps.android_modern_pl.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
