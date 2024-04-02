c0dab6c7c66158c00ed5ee7df82cdbd8ecd28836
Paulo Linhares <inf00174@sky.com.br>
2024-04-02 16:13:51 -0300

    libs

--- features/src/main/java/com/packapps/features/places/view/FilterDialogFragment.kt
+++ features/src/main/java/com/packapps/features/places/view/FilterDialogFragment.kt
@@ -56,15 +56,5 @@ class FilterDialogFragment : DialogFragment() {
         super.onDestroyView()
         _binding = null
     }
-    companion object {
 
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FilterDialogFragment().apply {
-                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
